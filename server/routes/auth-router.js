79cb0cc13ff57b1c1d24d6acfc48a6158b96355f
tolgabayrakdev <bayraktolga28@gmail.com>
2024-01-27 10:47:21 +0300

    rmv js

--- server/routes/auth-router.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import AuthController from '../controller/auth-controller.js';
-const authController = new AuthController();
-
-const router = express.Router();
-
-router.post('/login', authController.login);
-
-export default router;
