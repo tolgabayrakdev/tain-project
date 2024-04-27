e5ceb186647dbc7e3aceeaddd5f034c4de6c66db
tolgabayrakdev <bayraktolga28@gmail.com>
2024-04-27 16:15:53 +0300

    d

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 export default function Home() {
     return (
-        <div className="flex flex-col h-screen justify-center items-center bg-gradient-to-t from-indigo-50 to-blue-100">
+        <div className="flex flex-col h-screen justify-center items-center">
             <h3 className="text-4xl ">Welcome</h3>
             <Link
                 className="hover:text-blue-500 hover:underline duration-200"
